4ba930e983cb2b77bb46118e4136fce09928edbb
Erin Clark <erincodepirate@gmail.com>
2023-05-16 18:12:10 -0700

    Add pwa to camera page

--- src/app/home/camera/camera.page.ts
+++ src/app/home/camera/camera.page.ts
@@ -11,7 +11,7 @@ import { isPlatform } from '@ionic/angular';
 })
 export class CameraPage implements OnInit, OnDestroy {
 
-  selectImage: any;
+  selectImage: any = {};
 
   constructor() {
 
@@ -24,38 +24,65 @@ export class CameraPage implements OnInit, OnDestroy {
   }
 
   checkPlatformForWeb() {
-    if(Capacitor.getPlatform() == 'web') {
+    if (Capacitor.getPlatform() == 'web') {
       return true;
     }
     return false;
   }
 
-  async getPhoto() {
-    await Camera.requestPermissions();
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      source: CameraSource.Prompt,
-      width: 600,
-      resultType: CameraResultType.Uri
-    });
-  
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    //var imageUrl = image.webPath;
-  
-    // Can be set to the src of an image now
-    //imageElement.src = imageUrl;
-    this.selectImage = image;
+  async shareViaPlugin() {
+    let shareRet = await Share.share({
+      title: "Image Sharing",
+      text: "Share this Image",
+      url: this.selectImage?.dataUrl,
+      dialogTitle: "Image Sharing"
+    })
+  }
 
+  async nativeDeviceShare() {
     await Share.share({
       title: 'Image sharing',
       text: 'Share this image',
       url: this.selectImage.path,
       dialogTitle: 'Image share',
     });
+  }
+
+  srcToFile(src: RequestInfo | URL, fileName: string, mimeType: any) {
+    return (fetch(src)
+      .then(res => { return res.arrayBuffer(); }))
+      .then(buf => { return new File([buf], fileName, { type: mimeType }) })
+  }
+
+  async getPhoto() {
+    if (!this.checkPlatformForWeb()) {
+      const status = await Camera.requestPermissions();
+      console.log('status: ', status);
+      if (status.camera != 'granted' || status.photos != 'granted') return;
+    }
+    const image = await Camera.getPhoto({
+      quality: 90,
+      //allowEditing: true,
+      source: CameraSource.Prompt,
+      width: 600,
+      resultType: this.checkPlatformForWeb() ? CameraResultType.DataUrl : CameraResultType.Uri
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    //var imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+    this.selectImage = image;
+
+    if (this.checkPlatformForWeb())
+      this.shareViaPlugin();
+    else
+      this.nativeDeviceShare();
+
   }
 
 }
