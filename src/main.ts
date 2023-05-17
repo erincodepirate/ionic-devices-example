f4329371cd8af8d5a6159f136d08c544ca658c4d
Erin Clark <erincodepirate@gmail.com>
2023-05-16 17:26:03 -0700

    Add pwa elements

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+defineCustomElements(window);
